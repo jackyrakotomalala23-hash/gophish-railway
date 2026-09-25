f2e7973380cdff07c2d9255953dc722c3d8ccced
jackyrakotomalala23-hash <jacky.rakotomalala23@gmail.com>
2026-09-25 15:43:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-python3 relay.py &
-python3 -m http.server ${PORT:-8080}
+# Lancement du relais en arrière-plan avec les logs redirigés
+python3 relay.py > relay.log 2>&1 &
+
+# Lancement du serveur HTTP Python au premier plan sur le port de Railway
+exec python3 -m http.server ${PORT:-8080}
