a5d0c211f2837f5eaac4a288cd97d167c40facad
jackyrakotomalala23-hash <jacky.rakotomalala23@gmail.com>
2026-09-25 15:34:39 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-echo "Démarrage du script relay..."
 python3 relay.py &
-echo "Démarrage du serveur HTTP sur le port ${PORT:-8080}..."
-cd /app
-python3 -m http.server ${PORT:-8080}
+exec python3 -m http.server ${PORT:-8080}
