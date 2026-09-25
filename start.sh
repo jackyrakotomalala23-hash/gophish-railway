65c349e8288fa0b321648a657c2d68e879e4d254
jackyrakotomalala23-hash <jacky.rakotomalala23@gmail.com>
2026-09-25 15:50:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 python3 relay.py &
-python3 -c "
-import http.server
-import socketserver
-import os
-
-PORT = int(os.environ.get('PORT', 8080))
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(('0.0.0.0', PORT), Handler) as httpd:
-    print(f'Serveur web actif sur le port {PORT}')
-    httpd.serve_forever()
-"
+python3 -m http.server 8080
